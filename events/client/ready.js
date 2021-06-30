0d8a7d31e6ef07cd45a71f0feb6f679cab4dbcbb
um-bel <mienkojose@gmail.com>
2021-06-30 00:57:24 -0700

    ahh

--- events/client/ready.js
+++ events/client/ready.js
@@ -4,8 +4,8 @@ module.exports = async (Discord, client) =>{
     const clientMs = Math.round(client.ws.ping); 
     
     setTimeout(() => {
-        client.user.setActivity(`On ${clientMs} ping`, { type: "PLAYING"});
-    }, 500); 
+        client.user.setActivity(`on ${clientMs} ping`, { type: "PLAYING"});
+    }, 5000); 
 
     // const memberCounter = require('../../counters/member-counter'); 
 
